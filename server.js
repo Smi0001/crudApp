df8a6037f6da8f906d0243e5230464ca5f1cc5cd
DESKTOP-1KBVSNL\shamm <shammihans1@gmail.com>
2017-08-13 18:05:39 +0530

    commit ver2 --delete (single/multiple) quotes --update quote pending: UI loading, tick sign, error msg

--- server.js
+++ server.js
@@ -5,9 +5,11 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 var db;
-MongoClient.connect('mongodb://smi:smi1234@ds153392.mlab.com:53392/filmy-dialogues', (err, database) => {
+var url = 'mongodb://smi:smi1234@ds153392.mlab.com:53392/filmy-dialogues';
+MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
   db = database;
   app.listen(3000, () => {
@@ -15,27 +17,43 @@ MongoClient.connect('mongodb://smi:smi1234@ds153392.mlab.com:53392/filmy-dialogu
   });
 });
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+.use(bodyParser.urlencoded({extended: true}))
 .use(express.static(__dirname + '/static'))
-.get('/', (req, res) => {
+.get('/', (req, res) => {   
   db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err);
-    // renders index.ejs
-    //console.log('res'+result);
+    if (err) return console.log('--->Error->',err);
     //res.sendFile(__dirname + '/index.html');
     res.render('index.ejs', {quotes: result});
   });
 })
-.post('/quotes', (req, res) => {
+.post('/saveQuotes', (req, res) => {
   db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database');
+    if (err) return console.log('--->Error->',err);
+    console.log('--->saved to database');
     res.redirect('/');
   });
 })
-.delete('/del', function (req, res) {
-  res.send('DELETE request to homepage');
-  // res.redirect('/');
+.post('/deleteQuote', (req, res) => {
+  db.collection('quotes').remove({_id: new ObjectId(req.body._id)}, function(err, result) {
+    if (err) console.log('--->Error->',err);
+    console.log('--->deleted->', req.body._id);
+    res.send(req.body);                      
+  });
+})
+.post('/updateQuote', (req, res) => {
+  var _body = req.body;
+  console.log('_body', _body);
+  var ObjectId = require('mongodb').ObjectId;
+  var myquery = {"_id": new ObjectId(_body._id)};
+  var newvalues = { name: _body.name, quote: _body.quote };
+  db.collection('quotes').updateOne(
+    myquery,//query
+    newvalues, // replacement
+    (err, result) => {
+    if (err) console.log('--->Error->',err);
+    res.send(result);
+  });
 });
+
 console.log('The server started well TAAU :p');
